65b9418aebcf851af32d1bfdf8123474e5978a22
Michal Zmyslowski <schizoburger@gmail.com>
2017-09-15 17:08:48 +0200

    fixed bugsand adjusted Offspring

--- neat.py
+++ neat.py
@@ -11,51 +11,47 @@ class Genome(object):
         self.number_of_sensor_units = number_of_sensor_units
         self.number_of_output_units = number_of_output_units
 
-        self.__init_node_genes()
-        self.__init_connection_genes()
+        self.init_node_genes()
+        self.init_connection_genes()
 
         self.fitness = 0
 
-    def __init_node_genes(self):
+    def init_node_genes(self):
         self.node_genes = []
         for i in range(self.number_of_sensor_units):
-            self.mutate_add_node('sensor', i)
-        for i in range(self.number_of_output_units, elf.number_of_sensor_units+self.number_of_output_units)
+            self.add_node('sensor', i)
+        for i in range(self.number_of_sensor_units, self.number_of_sensor_units+self.number_of_output_units):
             self.add_node('output', i)
 
-    def __init_connection_genes(self):
+    def init_connection_genes(self):
         self.connection_genes = []
-        dim = number_of_sensor_units*number_of_output_units
+        dim = self.number_of_sensor_units+self.number_of_output_units
         self.connection_matrix = np.zeros((dim,dim))
         for node1 in self.node_genes:
             if node1['type']=='sensor':
                 for node2 in self.node_genes:
                     if node2['type']=='output':
                         self.add_connection(node1['id'], node2['id'])
-        # We now ensure that there will be no more direct connections
-        # between input and output nodes
-        for other in np.argwhere(self.connection_matrix==0):
-            self.connection_matrix[other[0],other[1]]=-1
-
 
     def add_node(self, node_type='hidden', node_id=None):
+        node_id = len(self.node_genes) if node_id == None else node_id
         new_node = {
-            'id': len(self.node_genes)-1 if node_id == None else node_id,
+            'id': node_id,
             'type': node_type,
-            'innovation': Genome.get_innovation_id(type='node_gene',id=node_id)
+            'innovation': Genome.get_innovation_id(gene_type='node',node_id=node_id)
         }
         self.node_genes.append(new_node)
         return new_node
 
     def add_connection(self, in_node_id, out_node_id, weight=None):
-        innovation_number = Genome.get_innovation_id(type='connection', in_node_id=in_node_id, out_node_id=out_node_id)
+        innovation_number = Genome.get_innovation_id(gene_type='connection', in_node_id=in_node_id, out_node_id=out_node_id)
         new_connection = {
             'in': in_node_id,
             'out': out_node_id,
             'weight': self.getRandomWeight() if weight == None else weight,
             'state': 'enabled',
             'innovation': innovation_number
-        })
+        }
         self.connection_genes.append(new_connection)
         self.connection_matrix[in_node_id,out_node_id] = innovation_number
         return new_connection
@@ -69,24 +65,39 @@ class Genome(object):
                 connection_to_split_weight=connection['weight']
                 connection['state']= 'disabled'
 
+        # We flag the connection as disabled by -1
+        self.connection_matrix[in_node_id,out_node_id]=-1
+
         new_connection_matrix = np.zeros((self.connection_matrix.shape[0]+1,self.connection_matrix.shape[1]+1))
         new_connection_matrix[:-1,:-1]=self.connection_matrix
+        self.connection_matrix = new_connection_matrix
 
         new_node = self.add_node()
-        self.add_connection(in_node_id, new_node['in'], 1)
-        self.add_connection(new_node['out'], out_node_id, connection_to_split_weight)
+        self.add_connection(in_node_id, new_node['id'], 1)
+        self.add_connection(new_node['id'], out_node_id, connection_to_split_weight)
 
     def mutate_add_connection(self):
-        in_node_id, out_node_id = self.get_random_unconnected_genes()
-        self.add_connection(in_node_id, out_node_id)
+        random_unconnected_genes = self.get_random_unconnected_genes()
+        # We check if there are some unconnected genes
+        if random_unconnected_genes!=None:
+            in_node_id, out_node_id = random_unconnected_genes
+            self.add_connection(in_node_id, out_node_id)
 
     def get_random_unconnected_genes(self):
-        connected_indices = np.argwhere(self.connection_matrix==0)
-        random_index = np.random.randint(0, connected_indices.shape[0])
-        return (connected_indices[random_index][0],connected_indices[random_index][1])
+        # We get indices of unconnected nodes
+        # with an exception of of sensor_units
+        # being treated as out
+        connected_indices = np.argwhere(self.connection_matrix[:,self.number_of_sensor_units:]==0)
+        if len(connected_indices)==0:
+            return None
+        else:
+            random_index = np.random.randint(0, connected_indices.shape[0])
+            return (connected_indices[random_index][0],connected_indices[random_index][1])
 
     def get_random_connected_genes(self):
-        connected_indices = np.argwhere((self.connection_matrix!=0) & (self.connection_matrix!=-1))
+        # -1 is for disabled
+        # 0 is for connected
+        connected_indices = np.argwhere((self.connection_matrix>0))
         random_index = np.random.randint(0, connected_indices.shape[0])
         return (connected_indices[random_index][0],connected_indices[random_index][1])
 
@@ -96,30 +107,40 @@ class Genome(object):
     def getRandomWeight(self):
         return np.random.randn()*np.sqrt(2.0/self.number_of_sensor_units)
 
+    def getMaxValueInConnectionGenesIds(self):
+        max_value = 0
+        # We look for the highsted id in offspring's connection_genes
+        for connection in self.connection_genes:
+            current_max_value=max(connection['in'],connection['out'])
+            if current_max_value>max_value:
+                max_value = current_max_value
+        return max_value
+
     @staticmethod
-    def get_innovation_id(type, id=None, in_node_id=None, out_node_id=None):
+    def get_innovation_id(gene_type, node_id=None, in_node_id=None, out_node_id=None):
         hasInnovation = False
         innovation_number = -1
         for innovation in Genome.innovations:
-            if innovation['type']=='node' and innovation['id']=id:
+            if gene_type=='node' and innovation['type'] == gene_type and innovation['node_id']==node_id:
                 hasInnovation=True
                 innovation_number=innovation['innovation_number']
-            elif: innovation['type']=='connection' and innovation['in']=in and innovation['out']=out:
+            elif gene_type=='connection' and innovation['type'] == gene_type and innovation['in']==in_node_id and innovation['out']==out_node_id:
                 hasInnovation=True
                 innovation_number=innovation['innovation_number']
         if hasInnovation==False:
             Genome.global_innovation_number+=1
             innovation_number = Genome.global_innovation_number
-            if innovation['type']=='node':
-                Genome.innovations.append([
+            if gene_type=='node':
+                Genome.innovations.append({
                     'type': 'node',
+                    'node_id': node_id,
                     'innovation_number': innovation_number
-                ])
+                })
             else:
                 Genome.innovations.append({
                     'type': 'connection',
-                    'in': in,
-                    'out': out,
+                    'in': in_node_id,
+                    'out': out_node_id,
                     'innovation_number': innovation_number
                 })
         return innovation_number
@@ -128,54 +149,62 @@ class Genome(object):
 class Offspring(Genome):
 
     def __init__(self, Parent1, Parent2):
-        self.connection_genes = self.__mate(Parent1, Parent2)
         self.number_of_sensor_units = Parent1.number_of_sensor_units
         self.number_of_output_units = Parent1.number_of_output_units
-        self.connection_matrix = self.__create_connection_matrix()
 
-    def __mate(self, Parent1, Parent2):
-        connection_genes = []
+        self.mate(Parent1, Parent2)
+        self.init_connection_matrix()
+        self.init_node_genes()
+
+
+
+    def mate(self, Parent1, Parent2):
+        self.connection_genes = []
         parent1_connection_genes = Parent1.connection_genes
         parent2_connection_genes = Parent2.connection_genes
         min_length = min([len(parent1_connection_genes),len(parent2_connection_genes)])
         i, j = (0,0)
         while i < min_length and j < min_length:
             # In this situation we either have matching genes or disjoint genes
-            if parent1_connection_genes[i]['Innovation']==parent2_connection_genes[j]['Innovation']:
+            if parent1_connection_genes[i]['innovation']==parent2_connection_genes[j]['innovation']:
                 # Now we are hadnling matching genes, choosing randomly one of them
                 random_index = np.random.randint(1, 3)
                 if random_index == 1:
-                    connection_genes.append(parent1_connection_genes[i])
+                    self.connection_genes.append(parent1_connection_genes[i])
                 elif random_index == 2:
-                    connection_genes.append(parent2_connection_genes[i])
+                    self.connection_genes.append(parent2_connection_genes[j])
                 i+=1
                 j+=1
-            elif parent1_connection_genes[i]['Innovation']<parent2_connection_genes[j]['Innovation']:
+            elif parent1_connection_genes[i]['innovation']<parent2_connection_genes[j]['innovation']:
                 # We are taking disjoint gene from parent1
-                connection_genes.append(parent1_connection_genes[i])
+                self.connection_genes.append(parent1_connection_genes[i])
                 i+=1
             else:
                 # We are taking disjoint gene from parent2
-                connection_genes.append(parent2_connection_genes[j])
+                self.connection_genes.append(parent2_connection_genes[j])
                 j+=1
-        if i<len(parent1_connection_genes)-1:
-            # We have some disjoint and excess genes in Parent1
-            connection_genes.extend(parent1_connection_genes[i:len(parent1_connection_genes)])
-        if j<len(parent2_connection_genes)-1:
-            # We have some disjoint and excess genes in Parent2
-            connection_genes.extend(parent2_connection_genes[j:len(parent2_connection_genes)])
-        return connection_genes
+        if i<len(parent1_connection_genes):
+            # We have some excess genes in Parent1
+            self.connection_genes.extend(parent1_connection_genes[i:len(parent1_connection_genes)])
+        if j<len(parent2_connection_genes):
+            # We have some excess genes in Parent2
+            self.connection_genes.extend(parent2_connection_genes[j:len(parent2_connection_genes)])
 
-    def __create_connection_matrix(self):
-        max_value = 0
+    def init_connection_matrix(self):
+        max_value = self.getMaxValueInConnectionGenesIds()
+        matrix_dim = int(max_value)+1
+        self.connection_matrix = np.zeros((matrix_dim,matrix_dim))
         for connection in self.connection_genes:
-            if max(connection['In'],connection['Out'])>max_value:
-                max_value = max(connection['In'],connection['Out'])
-        matrix_dim = int(max_value+1)
-        connection_matrix = np.zeros((matrix_dim,matrix_dim))
-        for connection in self.connection_genes:
-            connection_matrix[connection['In'],connection['Out']]=connection['Innovation']
-        return connection_matrix
+            self.connection_matrix[connection['in'],connection['out']]=connection['innovation']
+
+    def init_node_genes(self):
+        super(Offspring, self).init_node_genes()
+        max_value = self.getMaxValueInConnectionGenesIds()
+        start=self.number_of_sensor_units+self.number_of_output_units
+        for i in range(start, max_value):
+            self.add_node()
+
+
 
 class Species(object):
     species_list = []
@@ -247,17 +276,6 @@ class Neural_Network(object):
     def modified_sigmoid(x):
         return 1/(1+np.exp(-4.9*x))
 
-    #@staticmethod
-    #def forward(x, genome, start_iteration):
-    #    output_index = len(x)-1
-    #    output = 0
-    #    if start_iteration>=output_index:
-    #        for connection in genome.connection_genes:
-    #            if start_iteration==connection['Out'] and connection['State']=='Enabled':
-    #                output+=Neural_Network.forward(x, genome, connection['In'])*connection['Weight']
-    #        return Neural_Network.modified_sigmoid(output)
-    #    else:
-    #        return x[start_iteration]
     @staticmethod
     def forward(input, connection_genes):
         x=input
@@ -289,6 +307,23 @@ class Neural_Network(object):
                 connection['Iterated']=True
         return output
 
+gen1 = Genome(2,1)
+gen2 = Genome(2,1)
+gen1.mutate_add_node()
+gen2.mutate_add_node()
+kid = Offspring(gen1, gen2)
+print(gen1.node_genes)
+print(gen1.connection_genes)
+print('\n')
+print(gen2.node_genes)
+print(gen2.connection_genes)
+print('\n')
+print(kid.node_genes)
+print(kid.connection_genes)
+#print(kid.connection_genes)
+#print(kid.node_genes)
+#print(kid.connection_genes)
+#print(kid.connection_matrix)
 #gen = Genome(3,1)
 #weights=[]
 #inputs=[35,174, 287]
